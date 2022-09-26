cd28e2ec4ebd4c5374b9718117ded951ca3dcd80
Dio <diosaves@gmail.com>
2022-09-26 19:23:54 -0300

    ⚡️feat: Working Unity on Flutter

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_unity_widget/flutter_unity_widget_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterUnityWidgetPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterUnityWidgetPlugin"));
 }
